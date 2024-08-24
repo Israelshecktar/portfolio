12b7c7d7734298bd72653bb78a42ad8e3e62ce53
Israelshecktar <iamshecktar1996@gmail.com>
2024-08-24 18:17:55 +0100

    feat: Migrated completely from reactJS to NeXtjs

--- shecktar-portfolio/src/pages/project.js
+++ shecktar-portfolio/src/pages/project.js
@@ -1,4 +1,4 @@
-import ProjectCard from '../components/ProjectCard';
+import ProjectCard from '../components/Footer';
 import projects from '../data/projects';
 
 export default function Projects() {
